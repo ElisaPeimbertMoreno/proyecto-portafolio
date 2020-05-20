488d39fc9261db2d2202b83eee51f096b2acb380
ElisaPeimbertMoreno <61030388+ElisaPeimbertMoreno@users.noreply.github.com>
2020-05-20 14:26:06 -0500

    20_mayo

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById("nav-toggle");
+const subMenu = document.getElementById("sub-nav-toggle");
+const menuItems = menu.querySelector(".menu-items");
+const subMenuItems = subMenu.querySelector(".sub-menu-items");
+
+menu.addEventListener("click", menuToggler);
+subMenu.addEventListener("click", subMenuToggler);
+
+function menuToggler() {
+  menuItems.toggleAttribute("hidden");
+}
+
+function subMenuToggler() {
+  subMenuItems.toggleAttribute("hidden");
+}
